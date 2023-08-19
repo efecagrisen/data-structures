314e2ebbd01d4ff4c634ddc3773e1ff0a650dd5e
efecagrisen <efecagrisen@yahoo.com>
2023-08-19 18:50:28 +0300

    SetReview

--- day02-CollectionsReview/src/main/java/com/cydeo/SetReview.java
+++ day02-CollectionsReview/src/main/java/com/cydeo/SetReview.java
@@ -13,14 +13,27 @@ public class SetReview {
         // 2. add element
         set.add(new Student(1,"Jack"));
         set.add(new Student(2,"Julia"));
-        set.add(new Student(3,"Mike"));
         set.add(new Student(4,"Marry"));
+        set.add(new Student(3,"Mike"));
+        System.out.println(set.add(new Student(4, "Marry")));
 
         System.out.println(set);
+        String str = "Java Developer";
+        System.out.println(firstRepeatingChar(str));
 
     }
 
     public static Character firstRepeatingChar(String str){
+        //what is the time complexity of the following code? --> O(n)
+
+        // create a hashSet
+        Set<Character> chars = new HashSet<>(); // in here we have Space Complexity of O(n)
+
+        // iterate over the char array and add chars into hashSet
+        for (Character ch : str.toCharArray()) if (!chars.add(ch)) return ch;
+
+        // if add ops is false return that char
+
 
 
         return null;
