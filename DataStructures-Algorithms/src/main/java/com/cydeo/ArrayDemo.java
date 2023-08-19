7eda067b4b5a34ba0f26846c2b4766c30325a751
efecagrisen <efecagrisen@yahoo.com>
2023-08-19 12:01:46 +0300

    Data Structures and Algorithms

---
+++ DataStructures-Algorithms/src/main/java/com/cydeo/ArrayDemo.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] scoreArray = new int[10];
+
+        System.out.println(scoreArray); //[I@511baa65 --> starting point of the array for each index you add 4 (each int takes 4 bytes)
+
+    }
+}
